99e917c96c9bbfe92b6505f9f497f1e40d70e915
LunguRares <rareslungu@ymail.com>
2020-04-08 18:24:15 +0300

    Update RaresLungu2048.py

--- RaresLungu2048.py
+++ RaresLungu2048.py
@@ -10,13 +10,13 @@ import math
 import random
 
 def play_random_game(board):
-    firstMove = board.possible_moves()[random.randint(0,len(board.possible_moves())-1)]
+    firstMove = random.choice(better_possible_moves(board))
     board.make_move(firstMove)
-    score = board.random_rollout(math.inf)
+    score = board.better_random_rollout(board, 10)
     return [firstMove, score]
 
 def determine_next_move(board):
-    limit = 500
+    limit = 2000
     sums = [0, 0, 0, 0] # UP   DOWN    LEFT    RIGHT
     counts =   [0, 0, 0, 0]
     for i in range(0, limit):
@@ -101,4 +101,71 @@ def main():
 if __name__ == "__main__":
     main()
 
-  
+  def better_possible_moves(board):
+    possibilities = []
+    up_possible = False
+    down_possible = False
+    left_possible = False
+    right_possible = False
+    grid_state = board.export_state()
+    for y in range(4):
+        for x in range(4):
+            selected = grid_state[y][x]
+            down_neighbour = grid_state[y + 1][x] if y < 3 else -1
+            up_neighbour = grid_state[y - 1][x] if y > 0 else -1
+            left_neighbour = grid_state[y][x - 1] if x > 0 else -1
+            right_neighbour = grid_state[y][x + 1] if x < 3 else -1
+
+            if check_move(selected, up_neighbour):
+                up_possible = True
+            if check_move(selected, down_neighbour):
+                down_possible = True
+            if check_move(selected, left_neighbour):
+                left_possible = True
+            if check_move(selected, right_neighbour):
+                right_possible = True
+
+    if up_possible:
+        possibilities.append("UP")
+    if down_possible:
+        possibilities.append("DOWN")
+    if left_possible:
+        possibilities.append("LEFT")
+    if right_possible:
+        possibilities.append("RIGHT")
+    return possibilities
+
+
+def better_random_rollout(board, rounds):
+    gridstate = board.export_state()
+    possible_moves = better_possible_moves(board)
+    scores = [board.score, board.merge_count]
+    action = random.choice(possible_moves)
+    board.make_move(action)
+    while (not ((board.is_board_full() and board.possible_moves() == []) or rounds == 0)):
+        possible = better_possible_moves(board)
+        board.make_move(random.choice(possible))
+        board.add_random_tiles(1)
+        rounds -= 1
+    retscore = board.merge_count
+    board.grid = []
+    for row in gridstate:
+        copy2 = []
+        for elem in row:
+            if elem is None:
+                copy2.append(None)
+            else:
+                copy2.append(Tile(elem))
+        board.grid.append(copy2)
+    board.score = scores[0]
+    board.merge_count = scores[1]
+    return retscore
+
+
+def check_move(center, adjacent):
+    if center == adjacent:
+        if center is not None:
+            return True
+    if center is not None and center != -1 and adjacent is None:
+        return True
+    return False
